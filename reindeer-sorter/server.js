33fba86ff3e8422b02271896f13012bd0116c5aa
psforza <psforza@redhat.com>
2016-12-14 14:47:58 +0100

    sort in

--- reindeer-sorter/server.js
+++ reindeer-sorter/server.js
@@ -6,9 +6,20 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/', function(request, response){
-  console.log("Request Body: " + request.body);      // JSON Req
-  response.send(request.body);    // echo the Req
-  console.log("Response Body: " + response.body);      // JSON Req
+  console.log("Request Body: " + request.body);      // Log Req
+  var reqJson = JSON.parse(request.body);
+
+  function compare(a,b) {
+	  if (a.reindeerName < b.reindeerName) return -1;
+	  if (a.reindeerName > b.reindeerName) return 1;
+	  return 0;
+  }
+
+   reqJson.sort(compare);          
+  
+  response.body = JSON.stringify(reqJson);
+  response.send(response.body);                       // echo the Req
+  console.log("Response Body: " + response.body);    // Log Response
 });
 
 app.listen(8080);
