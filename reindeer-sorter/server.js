f6f04b5018aadfff131761dd4702a59cabc3811b
Pierluigi <psforza@redhat.com>
2016-12-14 14:14:54 +0100

    update

--- reindeer-sorter/server.js
+++ reindeer-sorter/server.js
@@ -1,39 +1,13 @@
-var cc               = require('config-multipaas'),
-    finalhandler     = require('finalhandler'),
-    http             = require("http"),
-    Router           = require('router'),
-    fs               = require('fs'),
-    serveStatic      = require("serve-static"),
-    express          = require('express');
+var express = require('express'),
+    bodyParser = require('body-parser');
 
-var config   = cc();
-var app      = Router()
+var app = express();
 
+app.use(bodyParser.json());
 
-// Serve up public/ftp folder 
-app.use(serveStatic('static'))
-
-// Routes
-app.use('/public', express.static('public'));
-app.get("/status", function (req, res) {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'application/json; charset=utf-8')
-  res.end("{status: 'ok'}\n")
-})
-
-app.get("/", function (req, res) {
-  var index = fs.readFileSync(__dirname + '/index.html')
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/html; charset=utf-8')
-  res.end(index.toString())
-})
-
-// Create server 
-var server = http.createServer(function(req, res){
-  var done = finalhandler(req, res)
-  app(req, res, done)
-})
-
-server.listen(config.get('PORT'), config.get('IP'), function () {
-  console.log( "Listening on " + config.get('IP') + ", port " + config.get('PORT') )
+app.post('/', function(request, response){
+  console.log(request.body);      // JSON Req
+  response.send(request.body);    // echo the Req
 });
+
+app.listen(3000);
